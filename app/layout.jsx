106b2c00e986dc27dd8011acfd1cc86534bdc0b8
Navneet <hunnyhunnyhunny@gmail.com>
2024-05-19 12:32:02 -0400

    fix build

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,6 +8,9 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "photoswipe/dist/photoswipe.css";
 
+
+export const dynamic = "force-dynamic";
+
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
   description: "Find your dream rental property",
