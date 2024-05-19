1805dd6badd16ad96eb34652ed2a30b69ee64525
Navneet <hunnyhunnyhunny@gmail.com>
2024-05-19 09:31:05 -0400

    Added Cloudinary Images

--- components/PropertyHeaderImage.jsx
+++ components/PropertyHeaderImage.jsx
@@ -6,7 +6,7 @@ const PropertyHeaderImage = ({ image }) => {
       <div className="container-xl m-auto">
         <div className="grid grid-cols-1">
           <Image
-            src={`/images/properties/${image}`}
+            src={image}
             alt=""
             width={0}
             height={0}
