e3a890e865293b49f340aff36106862708fa1ac8
Navneet <hunnyhunnyhunny@gmail.com>
2024-05-19 11:22:23 -0400

    ADdedMessages

---
+++ app/api/messages/route.js
@@ -0,0 +1,84 @@
+import connectDB from "@/config/database";
+import Message from "@/models/Message";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+export const dynamic = "force-dynamic";
+
+// GET /api/messages
+export const GET = async () => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.user) {
+      return new Response("User ID is required", {
+        status: 401,
+      });
+    }
+
+    const { userId } = sessionUser;
+
+    const readMessages = await Message.find({ recipient: userId, read: true })
+      .sort({ createdAt: -1 }) // Sort read messages in asc order
+      .populate("sender", "username")
+      .populate("property", "name");
+
+    const unreadMessages = await Message.find({
+      recipient: userId,
+      read: false,
+    })
+      .sort({ createdAt: -1 }) // Sort read messages in asc order
+      .populate("sender", "username")
+      .populate("property", "name");
+
+    const messages = [...unreadMessages, ...readMessages];
+
+    return Response.json(messages);
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
+
+// POST /api/messages
+export const POST = async (request) => {
+  try {
+    await connectDB();
+
+    const { name, email, phone, message, property, recipient } =
+      await request.json();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.user) {
+      return Response.json({
+        message: "You must be logged in to send a message",
+      });
+    }
+
+    const { user } = sessionUser;
+
+    // Can not send message to self
+    if (user.id === recipient) {
+      return Response.json({ message: "Can not send a message to yourself" });
+    }
+
+    const newMessage = new Message({
+      sender: user.id,
+      recipient,
+      property,
+      name,
+      email,
+      phone,
+      body: message,
+    });
+
+    await newMessage.save();
+
+    return Response.json({ message: "Message Sent" });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
