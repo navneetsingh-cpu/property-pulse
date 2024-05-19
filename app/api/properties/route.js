2f1788658be912e0a8df42797991bb5308837508
Navneet <hunnyhunnyhunny@gmail.com>
2024-05-19 08:44:45 -0400

    POST new property

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -11,3 +11,49 @@ export const GET = async (request) => {
     return new Response("something went wrong", { status: 500 });
   }
 };
+
+// POST /api/properties
+export const POST = async (request) => {
+  try {
+    await connectDB();
+
+    const formData = await request.formData();
+
+    const amenities = formData.getAll("amenities");
+    const images = formData
+      .getAll("images")
+      .filter((image) => image.name !== "");
+
+    const propertyData = {
+      type: formData.get("type"),
+      name: formData.get("name"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        state: formData.get("location.state"),
+        zipcode: formData.get("location.zipcode"),
+      },
+      beds: formData.get("beds"),
+      baths: formData.get("baths"),
+      square_feet: formData.get("square_feet"),
+      amenities,
+      rates: {
+        weekly: formData.get("location.weekly"),
+        monthly: formData.get("location.monthly"),
+        nightly: formData.get("location.nightly"),
+      },
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      images,
+    };
+
+    const properties = await Property.find({});
+    return new Response(JSON.stringify(properties), { status: 200 });
+  } catch (error) {
+    return new Response("something went wrong", { status: 500 });
+  }
+};
--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -90,7 +90,11 @@ const PropertyAddForm = () => {
 
   return (
     mounted && (
-      <form>
+      <form
+        action="/api/properties"
+        method="POST"
+        encType="multipart/form-data"
+      >
         <h2 className="text-3xl text-center font-semibold mb-6">
           Add Property
         </h2>
@@ -494,7 +498,7 @@ const PropertyAddForm = () => {
           <input
             type="text"
             id="seller_name"
-            name="seller_info.name."
+            name="seller_info.name"
             className="border rounded w-full py-2 px-3"
             placeholder="Name"
             value={fields.seller_info.name}
@@ -552,6 +556,7 @@ const PropertyAddForm = () => {
             accept="image/*"
             multiple
             onChange={handleImageChange}
+            required
           />
         </div>
 
