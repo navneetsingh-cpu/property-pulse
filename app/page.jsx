1788eff7c9674a7e892b45e30a60b21f5d4ebf28
Navneet <hunnyhunnyhunny@gmail.com>
2024-05-19 11:03:05 -0400

    Added search page

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import { Hero } from "@/components/Hero";
+import Hero from "@/components/Hero";
 import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
 
