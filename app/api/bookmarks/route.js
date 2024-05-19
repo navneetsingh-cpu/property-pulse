d0c3c2f1fe4ff5096e0e82d889525bd0477f94db
Navneet <hunnyhunnyhunny@gmail.com>
2024-05-19 10:51:01 -0400

    Bookmark feature

---
+++ app/api/bookmarks/route.js
@@ -0,0 +1,78 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+export const dynamic = "force-dynamic";
+
+// NOTE: here we need to send back a Content-Type: application/json response
+// header rather than a text/plain header.
+
+// GET /api/bookmarks
+export const GET = async () => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    // Find user in database
+    const user = await User.findOne({ _id: userId });
+
+    // Get users bookmarks
+    const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+
+    return Response.json(bookmarks);
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
+
+export const POST = async (request) => {
+  try {
+    await connectDB();
+
+    const { propertyId } = await request.json();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    // Find user in database
+    const user = await User.findOne({ _id: userId });
+
+    // Check if property is bookmarked
+    let isBookmarked = user.bookmarks.includes(propertyId);
+
+    let message;
+
+    if (isBookmarked) {
+      // If already bookmarked, remove it
+      user.bookmarks.pull(propertyId);
+      message = "Bookmark removed successfully";
+      isBookmarked = false;
+    } else {
+      // If not bookmarked, add it
+      user.bookmarks.push(propertyId);
+      message = "Bookmark added successfully";
+      isBookmarked = true;
+    }
+
+    await user.save();
+
+    return Response.json({ message, isBookmarked });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,17 +1,20 @@
 "use client";
-
-import PropertyDetails from "@/components/PropertyDetails";
-import PropertyHeaderImage from "@/components/PropertyHeaderImage";
-import PropertyImages from "@/components/PropertyImages";
-import Spinner from "@/components/Spinner";
-import { fetchProperty } from "@/utils/requests";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import { useState, useEffect } from "react";
+import { fetchProperty } from "@/utils/requests";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import PropertyDetails from "@/components/PropertyDetails";
+import PropertyImages from "@/components/PropertyImages";
+import BookmarkButton from "@/components/BookmarkButton";
+import PropertyContactForm from "@/components/PropertyContactForm";
+import ShareButtons from "@/components/ShareButtons";
+import Spinner from "@/components/Spinner";
 import { FaArrowLeft } from "react-icons/fa";
 
 const PropertyPage = () => {
   const { id } = useParams();
+
   const [property, setProperty] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -22,7 +25,7 @@ const PropertyPage = () => {
         const property = await fetchProperty(id);
         setProperty(property);
       } catch (error) {
-        console.error("Error fetching property", property);
+        console.error("Error fetching property:", error);
       } finally {
         setLoading(false);
       }
@@ -35,11 +38,12 @@ const PropertyPage = () => {
 
   if (!property && !loading) {
     return (
-      <h1 classNameName="text-center text-2xl font-bold mt-10">
+      <h1 className="text-center text-2xl font-bold mt-10">
         Property Not Found
       </h1>
     );
   }
+
   return (
     <>
       {loading && <Spinner loading={loading} />}
@@ -62,96 +66,17 @@ const PropertyPage = () => {
               <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
                 <PropertyDetails property={property} />
                 <aside className="space-y-4">
-                  <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-                    <i className="fas fa-bookmark mr-2"></i> Bookmark Property
-                  </button>
-                  <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-                    <i className="fas fa-share mr-2"></i> Share Property
-                  </button>
-
-                  <div className="bg-white p-6 rounded-lg shadow-md">
-                    <h3 className="text-xl font-bold mb-6">
-                      Contact Property Manager
-                    </h3>
-                    <form>
-                      <div className="mb-4">
-                        <label
-                          className="block text-gray-700 text-sm font-bold mb-2"
-                          htmlFor="name"
-                        >
-                          Name:
-                        </label>
-                        <input
-                          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                          id="name"
-                          type="text"
-                          placeholder="Enter your name"
-                          required
-                        />
-                      </div>
-                      <div className="mb-4">
-                        <label
-                          className="block text-gray-700 text-sm font-bold mb-2"
-                          htmlFor="email"
-                        >
-                          Email:
-                        </label>
-                        <input
-                          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                          id="email"
-                          type="email"
-                          placeholder="Enter your email"
-                          required
-                        />
-                      </div>
-                      <div className="mb-4">
-                        <label
-                          className="block text-gray-700 text-sm font-bold mb-2"
-                          htmlFor="phone"
-                        >
-                          Phone:
-                        </label>
-                        <input
-                          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                          id="phone"
-                          type="text"
-                          placeholder="Enter your phone number"
-                        />
-                      </div>
-                      <div className="mb-4">
-                        <label
-                          className="block text-gray-700 text-sm font-bold mb-2"
-                          htmlFor="message"
-                        >
-                          Message:
-                        </label>
-                        <textarea
-                          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 h-44 focus:outline-none focus:shadow-outline"
-                          id="message"
-                          placeholder="Enter your message"
-                        ></textarea>
-                      </div>
-                      <div>
-                        <button
-                          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center"
-                          type="submit"
-                        >
-                          <i className="fas fa-paper-plane mr-2"></i> Send
-                          Message
-                        </button>
-                      </div>
-                    </form>
-                  </div>
+                  <BookmarkButton property={property} />
+                  <ShareButtons property={property} />
+                  <PropertyContactForm property={property} />
                 </aside>
               </div>
             </div>
           </section>
-
           <PropertyImages images={property.images} />
         </>
       )}
     </>
   );
 };
-
 export default PropertyPage;
